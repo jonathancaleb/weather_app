312ebed1f46b28b5c39c3a089730dea81b1513e0
jonathancaleb <jonathancaleb230@gmail.com>
2023-12-22 15:45:44 +0300

    animations

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/service/weather_service.dart';
 
@@ -34,6 +35,32 @@ class _WeatherPageState extends State<WeatherPage> {
   }
 
   //weather animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sun.json';
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+
+      case 'clear':
+        return 'assets/sun.json';
+      default:
+        return 'assets/sun.json';
+    }
+  }
 
   //init state
   @override
@@ -47,14 +74,21 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[400],
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           //city
           Text(_weather?.cityName ?? "loading city.."),
 
+          //animation
+          Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
           //tem
-          Text('${_weather?.temperature}C')
+          Text('${_weather?.temperature} Celsius'),
+
+          //weather condition
+          Text(_weather?.mainCondition ?? "")
         ],
       ),
     );
